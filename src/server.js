e55c0546acb067fdfd99183919bb33179512866c
Joshua Wong <yayapied@gmail.com>
2018-06-16 15:06:31 +0800

    Used multer to handle file upload from the website

--- src/server.js
+++ src/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const async = require('async');
 const path = require('path');
 const morgan = require('morgan');
+const multer = require('multer');
+const tmp = require('tmp');
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const tmpdir = tmp.dirSync();
+const upload = multer({dest: tmpdir.name});
 const root = path.resolve(__dirname+'/../');
 
+// TODO: Use morgan to do logging
+
 // Serve static files
+const PORT = 5000 || process.env.PORT;
 app.use('/static', express.static(`${root}/assets/javascript`));
 
 app.get('/', async (req, res) => {
@@ -15,8 +21,11 @@ app.get('/', async (req, res) => {
   // TODO: CSRF Token? Though its probably unnecessary since theres no cookies or logins
 });
 
-app.post('/upload', async (req, res) => {
-  console.log (req);
+app.post('/upload', upload.single('mp4-upload'), async (req, res) => {
+  console.log('File:', req.file);
+  // TODO: Do conversion here
+  res.send('Placeholder');
+  // TODO: After conversion, remove file from tmp storage
 });
 
 console.log(`Server started on PORT ${PORT}`);
