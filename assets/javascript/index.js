d6e9c965e36d84c3ebeec4e6c6619c5dd71cea5d
Joshua Wong <yayapied@gmail.com>
2018-06-16 11:51:45 +0800

    Fixed some issues which prevented server from starting

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,11 +1,11 @@
-window.onload(() => {
+window.onload = () => {
   document.querySelector('#mp4-upload').addEventListener('change', () => {
     onUpload();
   });
-});
+};
 
 function validateFileType(filename) {
-  const allowedExtensions = ['mp4a', 'mp4'];
+  const allowedExtensions = ['mp4a', 'mp4', 'm4a'];
   fileType = filename.split('.').slice(-1)[0];
   // validate the file type
   if (allowedExtensions.indexOf(fileType) === -1) {
@@ -19,14 +19,15 @@ function onUpload() {
   if (file.value) {
     if (!validateFileType(file.value)) {
       document.querySelector('.warning-message').innerHTML = `Please ensure that you have submitted a mp4 or mp4a 
-      file`;
+      or m4a file`;
       return;
     }
   } else {
     document.querySelector('.warning-message').innerHTML = `Please ensure that you have submitted a file`;
+    return;
   }
   if (!validateFileSize(file)) {
-    document.querySelector('.warning-message').innerHTML = `Please ensure that the file is less than 10kb`;
+    document.querySelector('.warning-message').innerHTML = `Please ensure that the file is less than 40kb`;
     return;
   }
   document.querySelector('#mp4-upload-submit').disabled = false;
@@ -38,7 +39,7 @@ function validateFileSize(file) {
     alert("File API is not supported on your browser. Use a better one");
     return false;
   }
-  if (file.files[0].size > 10000) {
+  if (file.files[0].size > 40000) {
     return false;
   } 
   return true;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const async = require('async');
+const path = require('path');
 const morgan = require('morgan');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
+const root = path.resolve(__dirname+'/../');
+
+// Serve static files
+app.use('/static', express.static(`${root}/assets/javascript`));
 
 app.get('/', async (req, res) => {
-  res.send('./../assets/views/index.html');
+  res.sendFile(`${root}/assets/views/index.html`);
   // TODO: CSRF Token? Though its probably unnecessary since theres no cookies or logins
 });
 
 app.post('/upload', async (req, res) => {
   console.log (req);
-  // TODO: need to probably check what is being sent. Also should also get a linter
 });
 
 console.log(`Server started on PORT ${PORT}`);
